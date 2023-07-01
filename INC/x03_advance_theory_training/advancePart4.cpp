a3151dcbca6174715a5ccea273d18c86193d23dc
Wiktor <w.zawadzki@mail.com>
2023-07-01 13:10:30 +0200

    code update

--- INC/x03_advance_theory_training/advancePart4.cpp
+++ INC/x03_advance_theory_training/advancePart4.cpp
@@ -39,13 +39,13 @@ void CallBaseConstructorAndDeconstructor(void)
 void CallObjcectSlice(void)
 {
     DerivedObjSlice myDerivedObjSlice;
-    myDerivedObjSlice.dataD = 0x11;
-    myDerivedObjSlice.dataB = 0xFF;
+    myDerivedObjSlice.dataD = 1;
+    myDerivedObjSlice.dataB = 2;
     std::cout << "myDerivedObjSlice.dataB = " << myDerivedObjSlice.dataB << std::endl;
     std::cout << "myDerivedObjSlice.dataD = " << myDerivedObjSlice.dataD << std::endl;
 
 
-    BaseObjSlice myBaseObjSlice = myBaseObjSlice;
+    BaseObjSlice myBaseObjSlice = myDerivedObjSlice;
     std::cout << "myBaseObjSlice.dataB = " << myBaseObjSlice.dataB << std::endl;
 }
 //==========================================================================================================
@@ -53,14 +53,13 @@ void CallObjcectSlice(void)
 /***********************************************************************************************************
  * @brief   What is good way to count number of set bit using C++
  * 
- * 
  */
 void CallCountNumberInCpp(void)
 {
     int number, counter, i, tSize;
 
     tSize = sizeof(int) * 8;
-    number = 0xFF;
+    number = 0x7F;
     counter = 0x00;
     for(i=0; i<tSize; i++)
     {
@@ -70,7 +69,7 @@ void CallCountNumberInCpp(void)
     }
     std::cout << "Method1 Amount of set bits:= " << counter << std::endl;
 
-    number = 0xFF;
+    number = 0x7F;
     counter = 0x00;
     while(number)
     {
@@ -79,7 +78,7 @@ void CallCountNumberInCpp(void)
     }
     std::cout << "Metho2 Amount of set bits:= " << counter << std::endl;
 
-    number = 0xFF;
+    number = 0x7F;
     counter = 0x00;
     for(counter=0; number; ++counter)
     {// Britan Kernightan way
@@ -87,4 +86,95 @@ void CallCountNumberInCpp(void)
     }
     std::cout << "Metho3 Amount of set bits:= " << counter << std::endl;
 }// 3,32
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   What is override keyowrd in C++
+ * 
+ *          Testing beomes more easy with iths
+ *          Compile time check can be performed. Future error can be reduced
+ * 
+ *          void Function(void) override { std::cout << "Derived Class function " << std::endl; } points to
+ *          virtual void Function(void) { std::cout << "Base Class function " << std::endl; } and override tells explicid 
+ *          that this method need to be overrided
+ */
+void CallOverrideKey(void)
+{
+    BaseOverrideKey* pBaseOverrideKey = new DerivedOverrideKey();
+
+    pBaseOverrideKey->Function();
+}
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   What is placement new in C++
+ * 
+ *          In case when more object need to be alocated dynamic it is better to upront alocated moemory
+ *          For Kernel its is 
+ */
+void CallPlacementNew(void)
+{
+    // Normal case:
+    std::cout << "Normal case:" << std::endl;
+    BasePlecementNew* pBasePlecementNew = new BasePlecementNew(0x00);
+    delete pBasePlecementNew;
+
+    // Placement new case
+    std::cout << "Placement new case:" << std::endl;
+
+    uint8_t* pBase = new uint8_t[10 * sizeof(BasePlecementNew)]; // Memory pool
+
+    BasePlecementNew* pBasePlecementNew1 = new (pBase + 0) BasePlecementNew(0x00);
+    BasePlecementNew* pBasePlecementNew2 = new (pBase + 4) BasePlecementNew(0x04);
+    BasePlecementNew* pBasePlecementNew3 = new (pBase + 8) BasePlecementNew(0x08);
+
+    // Only reason to explicid call destructor in placement new
+    pBasePlecementNew1->~BasePlecementNew();
+    pBasePlecementNew2->~BasePlecementNew();
+    pBasePlecementNew3->~BasePlecementNew();
+
+    delete[] pBase;
+
+    // Drestruction neef to be done bacwards
+}
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   Reinterpreted cast
+ *          
+ *          It can perform dangerous conversation beacuse it can typecast ant pointer to ant other pointer
+ */
+void CallReinterpretedCast1(void)
+{
+    Golock* pGolock = new Golock();
+    Paranga* pParanga = new Paranga();
+
+    Golock* newGolock = reinterpret_cast<Golock*>(pParanga);
+
+    newGolock->Swing(9);
+
+    pGolock->Swing(2);
+    pParanga->Swing(1);
+}
+void CallReinterpretedCast2(void)
+{
+    someTool_t tTool;
+
+    tTool.x = 0x1100;
+    tTool.y = 0x3322;
+    tTool.z = 0x5544;
+    tTool.volume = 0x99887766;
+
+    uint8_t* pTool = reinterpret_cast<uint8_t*>(&tTool);
+
+    std::cout << "tTool.x = " << std::hex << *(reinterpret_cast<uint16_t*>(pTool + 0)) <<   std::endl;
+    std::cout << "tTool.y = " << std::hex << *(reinterpret_cast<uint16_t*>(pTool + 2)) <<   std::endl;
+    std::cout << "tTool.z = " << std::hex << *(reinterpret_cast<uint16_t*>(pTool + 4)) <<   std::endl;
+    std::cout << "tTool.volume = " << std::hex << *(reinterpret_cast<uint32_t*>(pTool + 8)) <<   std::endl;
+
+    std::cout << "tTool.x = " << std::hex << tTool.x <<   std::endl;
+    std::cout << "tTool.y = " << std::hex << tTool.y <<   std::endl;
+    std::cout << "tTool.z = " << std::hex << tTool.z <<   std::endl;
+    std::cout << "tTool.volume = " << std::hex << tTool.volume <<   std::endl;
+}
 //==========================================================================================================
--- INC/x03_advance_theory_training/advancePart4.h
+++ INC/x03_advance_theory_training/advancePart4.h
@@ -71,6 +71,120 @@ void CallObjcectSlice(void);
 /***********************************************************************************************************
  * @brief   What is good way to count number of set bit using C++
  * 
- * 
  */
 void CallCountNumberInCpp(void);
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   What is override keyowrd in C++
+ * 
+ *          Testing beomes more easy with iths
+ *          Compile time check can be performed. Future error can be reduced
+ * 
+ *          void Function(void) override { std::cout << "Derived Class function " << std::endl; } points to
+ *          virtual void Function(void) { std::cout << "Base Class function " << std::endl; } and override tells explicid 
+ *          that this method need to be overrided
+ */
+class BaseOverrideKey
+{
+    int _x;
+public:
+    virtual void Function(void) { std::cout << "Base Class function " << std::endl; }
+};
+
+class DerivedOverrideKey: public BaseOverrideKey
+{
+    int _y;
+public:
+    //void Function(void) { std::cout << "Derived Class function " << std::endl; }
+    void Function(void) override { std::cout << "Derived Class function " << std::endl; }
+    //void Function(int x) override { std::cout << "Derived Class function " << std::endl; }
+};
+
+void CallOverrideKey(void);
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   What is placement new in C++
+ * 
+ *          In case when more object need to be alocated dynamic it is better to upront alocated moemory
+ *          For Kernel its is 
+ */
+class BasePlecementNew
+{
+    int _offsetMem;
+public:
+    BasePlecementNew(int offsetMem)  
+    {   std::cout << "Base Class Constructor    ADDR + 0x" << std::hex << offsetMem << std::endl; 
+        _offsetMem = offsetMem;}
+
+    ~BasePlecementNew(void) 
+    {    std::cout << "Base Class Deconstructor  ADDR + 0x" << std::hex << _offsetMem << std::endl; }
+};
+void CallPlacementNew(void);
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   Reinterpreted cast
+ *          
+ *          It can perform dangerous conversation beacuse it can typecast ant pointer to ant other pointer
+ */
+class Golock
+{
+private:
+    int _n;
+public:
+    void Swing(int n)   
+    {   std::cout << "Golock swings " << n << " times" <<  std::endl; 
+        _n = n; }
+};
+
+class Paranga
+{
+private:
+    int _n;
+public:
+    void Swing(int n)   
+    {   std::cout << "Paranga swings " << n << " times" <<  std::endl; 
+        _n = n; }
+};
+
+typedef struct
+{
+    uint16_t x;
+    uint16_t y;
+    uint16_t z;
+    uint32_t volume;
+}someTool_t;
+
+void CallReinterpretedCast1(void);
+void CallReinterpretedCast2(void);
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   
+ * 
+ */
+
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   
+ * 
+ */
+
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   
+ * 
+ */
+
+//==========================================================================================================
+
+/***********************************************************************************************************
+ * @brief   
+ * 
+ */
+
+//==========================================================================================================
