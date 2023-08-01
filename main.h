4d6872095bd8974925bedc8b44a408c201492b9a
Wiktor <w.zawadzki@mail.com>
2023-08-01 21:47:48 +0200

    code load

---
+++ main.h
@@ -0,0 +1,33 @@
+#include "INC/x03_advance_theory_training/_x03_include.h"
+
+#ifdef _ADVANCE_PART_1_H
+#include "INC/x03_advance_theory_training/advancePart1.h"
+#endif
+
+#ifdef _ADVANCE_PART_2_H
+#include "INC/x03_advance_theory_training/advancePart2.h"
+#endif
+
+#ifdef _ADVANCE_PART_3_H
+#include "INC/x03_advance_theory_training/advancePart3.h"
+#endif
+
+#ifdef _ADVANCE_PART_4_H
+#include "INC/x03_advance_theory_training/advancePart4.h"
+#endif
+
+#ifdef _ADVANCE_PART_5_H
+#include "INC/x03_advance_theory_training/advancePart5.h"
+#endif
+
+#ifdef _ADVANCE_PART_6_H
+#include "INC/x03_advance_theory_training/advancePart6.h"
+#endif
+
+#ifdef _ADVANCE_PART_7_H
+#include "INC/x03_advance_theory_training/advancePart7.h"
+#endif
+
+#ifdef _CFILE_TEST_H
+#include "INC/x03_advance_theory_training/cfileTest.h"
+#endif
